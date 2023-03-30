1ed38ed3727cf5f5a8212d98dfd2834a100c491d
DongHwan Kim <jino0709@ajou.ac.kr>
2023-03-30 21:19:22 +0900

    Update make_query_mini.py

--- make_query_mini.py
+++ make_query_mini.py
@@ -1,7 +1,11 @@
+import os
+import sys
+import argparse
 import pandas as pd
 import numpy as np
 import sklearn.datasets as datasets
-
+import optuna
+from optuna import trial
 
  # return dataframe shape dim-0, dim-1 int data type
 def return_shape(df):
@@ -27,8 +31,8 @@ def column_type_check(df):
 	return numeric_col, categorical_col
 
 #class imbalance check function
-def class_imbalance_check(df, label):
-	label_count_list = df[label].value_counts().tolist()
+def class_imbalance_check(df, label_col):
+	label_count_list = df[label_col].value_counts().tolist()
 	flag = False
 	if max(label_count_list) > min(label_count_list) * 9:
 		flag = True
@@ -43,35 +47,39 @@ def make_df(data):
     df['target'] = data.target
     return df
 
-def make_query_classificaiton(data_path, selected_model, nan_check_flag, useless_features, numeric_col, categorical_col, label_col, class_cnt, 
-	class_imbalance_flag, scaler_flag):
+def make_query_ML_classificaiton(data_path, selected_model,  label_col, class_cnt, nan_check_flag, useless_features, numeric_col, categorical_col,
+	scaler_flag, class_imbalance_flag):
 	Query = f'Please make machine learning classification {selected_model} model using python.\nPlease generate the code in the order described below and set all SEED values (and random_state parameters) to 42.\n'
-
-	Query += f'Data path is {data_path}. Please load this data using pandas\n' #Data load
-	if nan_check_flagis not None:
-		Query += 'Data have nan value. Please fill nan data to zero\n' #fill nan
-	
-	if useless_feautres is not None:
-		Query += f'Data have useless features. Please drop {useless_features} columns.\n'#useless features drop
-
-	Query += 'Separate 10 percent of the validation data using the train_test_split function, and set the stratify parameter of the function to True\n' #train_test split
-
-	Query += f'Data has numeric columns such as {numeric_col}, has categoical columns such as {categorical_col}. Ignore if there is no categorical variable, else encode categorical columns using scikit-learn labelencoder\n' #labelencoding
-
-	if scaler_flag is not None:
-		Query += 'Scale data using scikit-learn MinMaxScaler.'
 
 	Query += f'This is data information. The label to be predicted is {label_col}, and the label consists of {class_cnt} types.\n'
 
+	Query += f'Data path is {data_path}. Please load this data using pandas\n' #Data load
+	if nan_check_flag is not False:
+		Query += 'Data have nan value. Please fill nan data to zero, But Remove columns with more than 20% missing values\n' #fill nan
+	
+	if useless_features is not False:
+		Query += f'Data have useless features. Please drop {useless_features} columns.\n'#useless features drop
 
-def make_query_regression(data_path, selected_model, nan_check_flag, useless_features, numeric_col, categorical_col, scaler_flag):
+	Query += 'Separate 10 percent of the validation data using the train_test_split function, and set the stratify parameter of the function to True\n' #train_test split
+
+	Query += f'Data has numeric columns such as {numeric_col}, has categoical columns such as {categorical_col}. Ignore if there is no categorical variable, else encode categorical columns using scikit-learn labelencoder.\n' #labelencoding
+
+	if scaler_flag is not False:
+		Query += 'Scale data using scikit-learn MinMaxScaler.\n'
+
+	if class_imbalance_flag is not False:
+		Query += 'Since there is a current class imbalance problem, use imblearn SMOTE library to oversample only the training data.\n'
+	return Query
+
+def make_query_ML_regression(data_path, selected_model, label_col, regression_cnt, nan_check_flag, useless_features, numeric_col, categorical_col, scaler_flag):
 	Query = f'Please make machine learning regression {selected_model} model using python\nPlease generate the code in the order described below and set all SEED values (and random_state parameters) to 42.\n'
+	Query += f'This is data information. The label to be predicted is {label_col}, and the label consists of {regression_cnt} types.\n'
 
 	Query += f'Data path is {data_path}. Please load this data using pandas\n' #Data load
-	if nan_check_flagis not None:
-		Query += 'Data have nan value. Please fill nan data to zero\n' #fill nan
+	if nan_check_flag is not None:
+		Query += 'Data have nan value. Please fill nan data to zero, But Remove columns with more than 20% missing values\n' #fill nan
 	
-	if useless_feautres is not None:
+	if useless_features is not None:
 		Query += f'Data have useless features. Please drop {useless_features} columns.\n'#useless features drop
 
 	Query += 'Separate 10 percent of the validation data using the train_test_split function, and set the stratify parameter of the function to True\n' #train_test split
@@ -80,6 +88,49 @@ def make_query_regression(data_path, selected_model, nan_check_flag, useless_fea
 
 	if scaler_flag is not None:
 		Query += 'Scale data using scikit-learn MinMaxScaler.'
+
+
+param_lgbm = {
+		'reg_lambda': 'trial.suggest_float("reg_lambda", 1e-5, 1.0)',
+		'reg_alpha': 'trial.suggest_float("reg_alpha", 1e-5, 1.0)',
+		'max_depth': 'trial.suggest_int("max_depth", 4,100)',
+		'colsample_bytree': 'trial.suggest_float("colsample_bytree", 0.1, 1)',
+		'subsample': 'trial.suggest_loguniform("subsample", 0.5, 1)',
+		'learning_rate': 'trial.suggest_loguniform("learning_rate", 1e-5, 1e-1)',
+		'n_estimators': 'trial.suggest_int("n_estimators", 100, 3000)',
+		'min_child_samples': 'trial.suggest_int("min_child_samples", 5, 100)',
+		'random_state' : 42
+		}
+param_xgb = {
+        'reg_lambda': 'trial.suggest_float("reg_lambda", 1e-5, 1.0)',
+        'reg_alpha': 'trial.suggest_float("reg_alpha", 1e-5, 1.0)',
+        'max_depth': 'trial.suggest_int("max_depth", 4,100)',
+        'colsample_bytree': 'trial.suggest_float("colsample_bytree", 0.1, 1)',
+        'subsample': 'trial.suggest_float("subsample", 0.5, 1)',
+        'learning_rate': 'trial.suggest_float("learning_rate",1e-5, 1e-1)',
+        'n_estimators': 'trial.suggest_int("n_estimators", 100, 3000)',
+        'min_child_weight': 'trial.suggest_int("min_child_weight", 1, 50)',
+        'gpu_id': -1,  # CPU 사용시
+		'random_state' : 42
+	}
+param_ex = {
+        'max_depth': 'trial.suggest_int("max_depth", 10, 100)',
+        'min_samples_split': 'trial.suggest_int("min_samples_split", 2, 150)',
+        #'max_leaf_nodes': trial.suggest_int('max_leaf_nodes', 2, 1000),
+        'n_estimators': 'trial.suggest_int("n_estimators", 100, 2500)',
+        'criterion' : 'trial.suggest_categorical("criterion", ["gini","entropy"])',
+        'bootstrap' : True,
+        'random_state': 42,
+   	}
+param_rf = {
+        'max_depth': 'trial.suggest_int("max_depth", 10, 100)',
+        'min_samples_split': 'trial.suggest_int("min_samples_split", 2, 150)',
+        #'max_leaf_nodes': trial.suggest_int('max_leaf_nodes', 2, 1000),
+        'n_estimators': 'trial.suggest_int("n_estimators", 100, 2500)',
+        'criterion' : 'trial.suggest_categorical("criterion", ["gini","entropy"])',
+        'random_state': 42,
+   	}
+
 
 
 
@@ -87,7 +138,7 @@ prob_type = None #'Classification' or 'Regression'
 selected_model = None
 
 
-iris = load_iris()
+'''iris = load_iris()
 diabetes = load_diabetes()
 digits = load_digits()
 linnerud = load_linnerud()
@@ -95,58 +146,62 @@ wine = load_wine()
 cancer = load_breast_cancer()
 
 ## data load
-df = make_df(iris)
+df = make_df(iris)'''
 
-prob_type = 'Classification'
-selected_model = 'RandomForest'
+if __name__ == '__main__':
 
+	prob_type = 'Classification'
+	selected_model = ['RandomForest','ExtraTree','XGBoost','LightGBM']
 
-if prob_type == 'Classification':
-	data_path = None
-	nan_check_flag = None
-	useless_features = None
-	numeric_col = None
-	categorical_col = None
-	label_col = None
-	class_cnt = None
-	class_imbalance_flag = None
-	scaler_flag = True
+	if prob_type == 'Classification':
+		data_path = None
+		nan_check_flag = None
+		useless_features = None
+		numeric_col = None
+		categorical_col = None
+		label_col = None
+		class_cnt = None
+		class_imbalance_flag = None
+		scaler_flag = True
 
-	#pca_flag = None
+		#pca_flag = None
 
+		data_path = './iris.csv'
+		label_col = 'Species'
+		useless_features = ['Id']
 
-	label_col = 'target'
-	nan_check_flag = nan_check(df)
+		df = pd.read_csv(data_path)
+		nan_check_flag = nan_check(df)
+		#Drop useless columns
+		if useless_features is not None:
+			df.drop(useless_features, axis=1 ,inplace=True)
+		class_cnt = class_cnt_calculate(df, label_col)
+		class_imbalance_flag = class_imbalance_check(df, label_col)
+		df.drop(label_col, axis=1, inplace=True)
+		numeric_col, categorical_col = column_type_check(df)
+		for model in selected_model:
+			Query = make_query_ML_classificaiton(data_path, model, label_col, class_cnt, nan_check_flag, useless_features, numeric_col, categorical_col, scaler_flag, class_imbalance_flag)
+			print(Query)
 
-	#Drop useless columns
-	if useless_featues is not None:
-		df.drop(useless_features, axis=1 ,inplace=True)
-	df.drop(label_col, axis=1, inplace=True)
-	class_cnt = class_cnt_calculate(df, label_col)
-	numeric_col, categorical_col = column_type_check(df)
-	class_imbalance_flag = class_imbalance_flag(df)
+	else: #Regression
+		data_path = None
+		nan_check_flag = None
+		useless_features = None
+		numeric_col = None
+		categorical_col = None
+		regression_cnt = None
+		label_col = None
+		scaler_flag = True
+		
+		label_col = ['target'] #Consider multivariate regression
+		nan_check_flag = nan_check(df)
 
-
-
-else: #Regression
-	data_path = None
-	nan_check_flag = None
-	useless_features = None
-	numeric_col = None
-	categorical_col = None
-	regression_cnt = None
-	label_col = None
-	scaler_flag = True
-	
-	label_col = 'target'
-	nan_check_flag = nan_check(df)
-
-	#Drop useless columns
-	if useless_featues is not None:
-		df.drop(useless_features, axis=1 ,inplace=True)
-	df.drop(label_col, axis=1, inplace=True)
-	class_cnt = class_cnt_calculate(df, label_col)
-	numeric_col, categorical_col = column_type_check(df)
+		#Drop useless columns
+		if useless_features is not None:
+			df.drop(useless_features, axis=1 ,inplace=True)
+		df.drop(label_col, axis=1, inplace=True)
+		regression_cnt = len(label_col)
+		numeric_col, categorical_col = column_type_check(df)
 
 
 
